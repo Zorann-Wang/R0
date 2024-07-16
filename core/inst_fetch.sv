2183b8ffd263eaf2c9343a3934fc884f74458795
Zorann-Wang <zorann.wang@gmail.com>
2024-07-16 14:36:18 +0100

    sb hewochaojia

---
+++ core/inst_fetch.sv
@@ -0,0 +1,15 @@
+module inst_fetch #(
+    
+)
+(
+    input   wire                     clk_i,
+    input   wire                     rst_n_i,
+);
+
+always_ff @( posedge clk_i ) begin 
+    if (conditions) begin
+        
+    end
+end
+
+endmodule
